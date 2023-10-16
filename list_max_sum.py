dbd80548523d63f90250eea7aea7f9d46b130271
Estphu <jaydee20001217@gmail.com>
2023-10-16 23:45:48 +0500

    list_max_sum file added

--- list_max_sum.py
+++ list_max_sum.py
@@ -1,7 +1,25 @@
 def list_max_sum(numbers):
-    pass
+    sum = []
+    max_sum = [0]
+    for number in numbers:
+        if number != numbers[0]:
+            sum.append(numbers[0] + number)
+        if number != numbers[1]:
+            sum.append(numbers[1] + number)
+        if number != numbers[2]:
+            sum.append(numbers[2] + number)
+        if number != numbers[3]:
+            sum.append(numbers[3] + number)                   
+    for i in sum:
+        for k in max_sum:
+            if (i > k):
+                max_sum.append(i)
+    return max_sum            
 
 
-if __name__ == 'main':
+            
+
+if __name__ == '__main__':
     print(list_max_sum([3,5,9,11]))
+    
 
