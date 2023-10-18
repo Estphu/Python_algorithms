0072235740c76c201b55054974ac16dfa267ac8d
Estphu <jaydee20001217@gmail.com>
2023-10-18 22:40:50 +0500

    added monkey_keyboard.py logic

---
+++ monkey_on_keyboard.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_random_alphabets(sentence):
+    sentence_alphabets = list(sentence)
+    matched_alphabets = []
+
+    while matched_alphabets != sentence_alphabets:  
+        for sentence_alphabet in sentence_alphabets:
+            if sentence_alphabet == ' ':
+                matched_alphabets.append(' ')
+                continue
+            alphabet = ''
+            while sentence_alphabet != alphabet:
+                alphabet = random.choice(string.ascii_letters)
+                if sentence_alphabet == alphabet:
+                    matched_alphabets.append(alphabet)
+                          
+
+    return f"MONKEY CATCHES YOUR SENTENCE\n"+''.join(matched_alphabets)     
+
+
+if __name__ == '__main__':
+    sentence = input('Enter sentence to get matched by monkey (only letters)\n')
+    print(generate_random_alphabets(sentence))    
