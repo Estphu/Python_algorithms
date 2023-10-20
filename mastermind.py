b6845ed418274a9021a1db76769bff9821dc469a
Estphu <jaydee20001217@gmail.com>
2023-10-21 00:41:56 +0500

    added mastermind.py (console game logic)

--- mastermind.py
+++ mastermind.py
@@ -1,30 +1,44 @@
-from random import randrange
+import random 
 
 def four_digit_mastermind():
-    random_four_digits = []
-    for i in range(4):
-        random_four_digits.append(randrange(9))
-    print(random_four_digits)    
-    for i in range(10):
-        result = []
+    random_four_digits = list(map(str, random.sample(range(0,9),4)) )
+    print('''
+            *** WELCOME TO MASTERMIND *** \n
+            GUESS THE FOUR DIGITS \n
+            ---MANUAL---\n
+            NUMBERS ARE BETWEEN 0 to 9\n
+            EACH NUMBER IS UNIQUE\n
+            THREE CHARACTERS AGAINST YOUR GUESSED NUMBER MEANINGS:\n
+            B ----> YOU GUESSED IT WRONG\n
+            Y ----> YOU GUESSED THE NUMBER RIGHT BUT IT'S IN WRONG PLACE\n
+            R ----> YOU GUESSED THE NUMBER RIGHT AND IT'S IN RIGHT PLACE\n
+            *** GOOD LUCK *** YOU GOT EIGHT TRIES
+     ''')
+    for i in range(8):
+        result = ['B','B','B','B']
         user_guess_nums = input('Enter your four digit guess code: ')
         user_guess_lists = list(user_guess_nums)
         user_guess_nums_len = user_guess_nums.__len__()
-        if user_guess_nums_len == 4: 
-            for random_four_digit in random_four_digits:
-                for user_guess_num in user_guess_lists:
+        if user_guess_nums_len == 4:        
+            for user_guess_num in user_guess_lists:
+                for random_four_digit in random_four_digits:
+                    user_index = user_guess_lists.index(user_guess_num)
+                    guess_index = random_four_digits.index(random_four_digit)
                     if user_guess_num == random_four_digit:
-                        if user_guess_lists.index(user_guess_num) == random_four_digits.index(random_four_digit):
-                            result.append('R')
+                        if user_index == guess_index:
+                            result[user_index] = 'R'
                         else:
-                            result.append('Y')
-                    else:
-                        result.append('B')
-                     
+                            result[user_guess_lists.index(user_guess_num)] = 'Y'
+
+            print(result)         
+            if result == ['R','R','R','R']:
+                print(''.join(result)) 
+                print('>>> CONGRATULATIONS IT MATCHED <<<')
+                break
         else:
             print("FOUL!!! PLEASE ENTER FOUR DIGITS")
             continue            
-        print(''.join(result))               
+                      
                                     
 
 if __name__ == '__main__':
